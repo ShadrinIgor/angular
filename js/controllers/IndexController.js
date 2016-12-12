66a63035327c5fd6a854d85dd335d64adc95fddf
Shadrin Igor <shadrin.igor@gmail.com>
2016-12-13 04:08:35 +0500

    Working with list page

--- js/controllers/IndexController.js
+++ js/controllers/IndexController.js
@@ -1,5 +1,15 @@
 mainApp.controller('IndexController',
-    function indexController($scope, $http, $rootScope) {
+    function IndexController($scope, $http, $rootScope) {
+
+        $http({method: 'GET', url: 'data.json'})
+            .then(
+                function (result) {
+                    $scope.centerData = result.data;
+                },
+                function (data, status, headers, config) {
+                    alert( "Have a http error" );
+                }
+            );
 
     }
 )
