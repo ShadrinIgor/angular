70b32bcffd83cbdd20a9e64d20f633ef302a97b2
Shadrin Igor <shadrin.igor@gmail.com>
2016-12-13 16:42:05 +0500

    Working with filter

--- js/controllers/IndexController.js
+++ js/controllers/IndexController.js
@@ -1,6 +1,20 @@
 mainApp.controller('IndexController',
     function IndexController($scope, $http, $rootScope) {
 
+        $scope.filter = {price_1: 0, price_2: 0, category: 0, name: ''};
+
+        $scope.filterItems = function( item ) {
+            var cout = true;
+
+            if( cout && $scope.filter.price_1 > 0 && item.price < $scope.filter.price_1 )cout = false;
+
+            if( cout && $scope.filter.price_2 > 0 && item.price > $scope.filter.price_2 )cout = false;
+            
+            if( cout && $scope.filter.category > 0 && item.category != $scope.filter.category )cout = false;
+
+            return cout;
+        }
+        
         $http({method: 'GET', url: 'data.json'})
             .then(
                 function (result) {
